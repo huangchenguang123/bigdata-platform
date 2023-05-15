96aa5893d61018c06f848815586b53fb858f73a6
chuxing <923622908@qq.com>
2023-05-15 16:42:12 +0800

    [feature]-[0.0.1-SNAPSHOT] 1.文件上传框完成

--- web-ui/src/components/UploadFile/index.tsx
+++ web-ui/src/components/UploadFile/index.tsx
@@ -1,58 +1,52 @@
 import React, {useContext} from 'react';
 import styles from './index.less';
 import classnames from 'classnames';
-import Button from '@/components/Button';
 import {
   Modal,
-  Form
+  UploadProps, message
 } from 'antd';
 import Dragger from "antd/es/upload/Dragger";
 import {FileManagerContext} from "@/context/file-manager";
+import i18n from "@/i18n";
 
 function VisiblyUploadFile() {
-  const [form] = Form.useForm();
   const { model, setIsUploaderShow } = useContext(FileManagerContext);
   const isUploaderShow = model.isUploaderShow;
   const handleOk = () => {
     setIsUploaderShow(false);
   };
+  const handleCancel = () => {
+    setIsUploaderShow(false);
+  };
+
+  const props: UploadProps = {
+    name: 'data',
+    multiple: false,
+    action: 'http://localhost:8080/csv/uploadTable',
+    onChange(info) {
+      const { status } = info.file;
+      if (status === 'done') {
+        message.success(`${info.file.name}` + i18n('file.upload.success')).then(() => {});
+      } else if (status === 'error') {
+        message.error(`${info.file.name}` + i18n('file.upload.error')).then(() => {});
+      }
+    }
+  };
 
   return <div className={classnames(styles.box)}>
     <Modal
-      title={"上传文件"}
+      title={i18n('file.upload.model.title')}
       open={isUploaderShow}
       onOk={handleOk}
+      onCancel={handleCancel}
       footer={false}
       width={560}
     >
-      <Form
-        form={form}
-        autoComplete="off"
-        className={styles.form}
-      >
-        <div>
-          <Dragger>
-            <p className="ant-upload-drag-icon">
-            </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-              band files
-            </p>
-          </Dragger>
-        </div>
-        <div className={styles.formFooter}>
-          <div className={styles.test}>
-            {
-              // !dataSourceId &&
-              <Button
-                className={styles.test}>
-                上传
-              </Button>
-            }
-          </div>
-        </div>
-      </Form>
+      <div>
+        <Dragger {...props}>
+          <p className="ant-upload-text">{i18n('file.upload.model.text')}</p>
+        </Dragger>
+      </div>
     </Modal>
   </div >
 
--- web-ui/src/i18n/zh-cn.ts
+++ web-ui/src/i18n/zh-cn.ts
@@ -1,7 +1,6 @@
 export default {
   lang: 'zh-cn',
 
-  'common.data.hour': '{1}小时',
   'common.data.minute': '{1}分钟',
   'common.tip.yesterday': '昨天{1}',
   'common.tip.tomorrow': '明天{1}',
@@ -9,38 +8,23 @@ export default {
   'common.tip.later': '后',
   'common.tip.now': '现在',
   'common.tip.justNow': '刚刚',
-  'common.text.search': '搜索',
-  'common.placeholder.select': '请选择{1}',
-  'common.text.serviceStarting': '服务启动中...',
-  'common.text.serviceFail': '服务启动失败',
-  'common.text.column': '列',
-  'common.text.indexes': '索引',
-  'common.button.save': '保存',
-  'common.button.execute': '执行',
   'common.text.background': '背景',
   'common.text.language': '语言',
   'common.text.en': '英文',
   'common.text.zh-cn': '中文',
 
   'home.nav.database': '连接管理',
-  'home.nav.myHistory': '历史记录',
-  'home.nav.github': 'GitHub',
-  'home.text.visitedTime': '最近访问时间：',
   'home.nav.file': '文件管理',
 
-  'history.title.myHistory': '我的SQL',
-  'history.tab.mySave': '我的保存',
-  'history.tab.executionHistory': '执行记录',
-
-  'home.title.name': '数据库',
-  'home.title.link': '连接',
-  'home.title.setting': '设置',
-  'home.title.query': '查询',
-
-  'connection.buttonn.colorSwitch': '颜色切换',
   'connection.button.overview': '连接概览',
   'connection.input.newLink': '新建连接',
   'connection.button.edit': '修改连接',
   'connection.button.clone': '克隆连接',
-  'connection.button.delete': '删除连接'
+  'connection.button.delete': '删除连接',
+
+  'file.upload.model.title': '上传文件',
+  'file.upload.model.text': '单击或拖动文件到此区域进行上传',
+  'file.upload.success': '上传成功',
+  'file.upload.error': '上传失败',
+
 }
